22c3b47a11ace88eaf711dc4cb389e6f35ede7cc
John Lee <yongjohnlee80@outlook.com>
2024-10-14 00:29:16 +0900

    codelldb configuration changed for macOS

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -164,7 +164,7 @@ local plugins = {
 			local codelldb = mason_registry.get_package "codelldb"
 			local extension_path = codelldb:get_install_path() .. "/extension/"
 			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 			local cfg = require "rustaceanvim.config"
 
 			vim.g.rustaceanvim = {
