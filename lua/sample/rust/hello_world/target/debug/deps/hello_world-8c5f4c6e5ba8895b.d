fe5a73c2b3d9ac7fd4471fb109825cae45339df1
johno <yongjohnlee80@outlook.com>
2024-10-12 22:34:17 -0700

    gitignore for rust target folder

--- lua/sample/rust/hello_world/target/debug/deps/hello_world-8c5f4c6e5ba8895b.d
+++
@@ -1,5 +0,0 @@
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/hello_world-8c5f4c6e5ba8895b: src/main.rs
-
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/hello_world-8c5f4c6e5ba8895b.d: src/main.rs
-
-src/main.rs:
--- lua/sample/rust/hello_world/target/debug/deps/hello_world-bb99aff441b518c5.d
+++
@@ -1,9 +0,0 @@
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/libhello_world-bb99aff441b518c5.rmeta: src/main.rs Cargo.toml
-
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/hello_world-bb99aff441b518c5.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
