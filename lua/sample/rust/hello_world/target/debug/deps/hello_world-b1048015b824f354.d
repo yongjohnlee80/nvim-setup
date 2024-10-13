33cefbb7e41bfcbf760f90757839bd6e6892c32e
johno <yongjohnlee80@outlook.com>
2024-10-13 01:52:25 -0700

    removing all target files

--- lua/sample/rust/hello_world/target/debug/deps/hello_world-b1048015b824f354.d
+++
@@ -1,5 +0,0 @@
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/hello_world-b1048015b824f354: src/main.rs
-
-/home/johno/.config/nvim/lua/sample/rust/hello_world/target/debug/deps/hello_world-b1048015b824f354.d: src/main.rs
-
-src/main.rs:
